44fbcf34599da7d1c6ba15af8a83543b0b5af886
bcnskaa <bcnskaa@gmail.com>
2014-08-22 16:05:38 +0800

    Updated.

--- src/plot_boxplot_from_qiime.R
+++ src/plot_boxplot_from_qiime.R
@@ -1,4 +1,3 @@
-# 2014 SKWoolf bcnskaa AT gmail DOT com
 library(ggplot2)
 library(reshape)
 
@@ -57,7 +56,7 @@ melt_df <- function(df_list, new_class_label="class", ignore_na=TRUE) {
 		print(paste("Processing ", label, "...", sep=""));
 		
 		cur_df <- df_list[[label]];
-		cur_df <- cbind(cur_df, iteration=seq(1, nrow(cur_df)))
+		cur_df <- cbind(cur_df, iteration=seq(1, nrow(cur_df)), stringsAsFactors=F)
 		
 		# Melt cur_df by holding iteration as an id
 		mdf <- melt(cur_df, id=c("iteration"));
@@ -75,16 +74,22 @@ melt_df <- function(df_list, new_class_label="class", ignore_na=TRUE) {
 	
 	colnames(melted_df)[colnames(melted_df) == "X______X_____X"] <- new_class_label;
 	
+	print("Merging done.");
+	
 	return(melted_df);
 }
 
-
-main2 <- function(group_info_fn, path, mean_replicate=FALSE)
+# Usage: 
+# main2("group.info", "/disk/rdisk08/yyjia3/16S/All_samples/QIIME_output_forward/bacteria/arare_min_P_1000/alpha_div_collated", "Bacteria")
+# main2("group.info", "/disk/rdisk08/yyjia3/16S/All_samples/QIIME_output_forward/archaea/arare_min_P_1000/alpha_div_collated", "Archaea")
+main2 <- function(group_info_fn, path, output_prefix=character(0), mean_replicate=FALSE)
 {
 	alpha_diversities <- c("PD_whole_tree", "chao1", "fisher_alpha", "shannon", "observed_species");
+	
 	# Discard some columns from the datasets
 	col_to_be_removed <- c("X", "sequences.per.sample", "iteration", "H.GZ", "H.SHX");
 
+	# Group info
 	group_info <- import_group_info(group_info_fn);
 
 	# Import the data
@@ -112,10 +117,10 @@ main2 <- function(group_info_fn, path, mean_replicate=FALSE)
 	cat("The following labels are set to be excluded from further analyses.\n");
 	excluded_list <- group_info$Label[which(group_info$Excluded == "1")];
 	cat("\t");cat(paste(excluded_list,sep=",", join="  "));cat("\n");
-	
-	
+
 	
 	cat(paste("Checking values...\n", sep=""));
+
 	# Raise warning message if the dataset contains N/A
 	for(alpha_diversity in alpha_diversities)
 	{
@@ -130,220 +135,198 @@ main2 <- function(group_info_fn, path, mean_replicate=FALSE)
 			
 			if(length(which(is.na(d[[label]]))) > 0)
 			{
-				cat(paste("Warning: ", alpha_diversity, "/", label, " contains N/A values\n", sep=""));
+				cat(paste("Warning: ", alpha_diversity, "/", label, " contains N/A values, and is dicarded from further analyses\n", sep=""));
 			}
 		}
 		
-		
 		dfs[[alpha_diversity]] <- d;
-		
 	}
 	
 	# Reshape dataframe
 	melt_dfs <- melt_df(dfs);
-	
-	# Discard rows with n/a values
-	melt_dfs <- melt_dfs[-which(is.na(melt_dfs$value)), ];
 
 	# Go through all alpha diversities
 	for(alpha_diversity in alpha_diversities)
 	{
 		# Select data on basis of selected alpha diversity
-		ss <- melt_dfs[melt_dfs$alpha_diversity == alpha_diversity, ];
+		ss <- melt_dfs[melt_dfs$class == alpha_diversity, ];
 		
-		
-		
-		
-		
-		#melt_ss <- melt(ss, id=c("sample", "experiment", "temperature"))
-		ss <- cbind(ss, id=paste(ss$sample, ss$experiment, ss$temperature, sep=":"))
-		
-		# Take a mean value from replicates
+		# Take a mean value from all iterations
 		if(flag_mean)
-		{
-			require(plyr)
-			ss <- ddply(ss, colnames(ss)[which(colnames(ss) != "value")], summarise, value=mean(value))
+		{	
+			require(plyr);
+			ss <- ddply(ss, .(variable, class), summarise, value=mean(value));	
 		}
+	
 		
-		pdf(paste("combined_replicates.", alpha_diversity, ".", kingdom,".boxplot.pdf", sep=""), width=4, height=6);
-		print(qplot(factor(id), value, data = ss, geom = "boxplot") + geom_jitter(position=position_jitter(w=0.1, h=0.1)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)))
-		# qplot(factor(variable), value, data = ss, geom = "boxplot", fill=factor(alpha_diversity)) + geom_jitter(position=position_jitter(w=0.1, h=0.1)) 
-		#p <- ggplot(ss, aes(factor(variable), value)) + geom_jitter();
-		#p + geom_boxplot(aes(fill=factor(alpha_diversity))) + theme(axis.text.x = element_text(angle = 90, hjust = 1));
+		# Assigning meta info to the ss
+		ss <- merge(ss, group_info, by.y="Label", by.x="variable");
+		ss <- cbind(ss, id=paste(ss$Place, " ", ss$Treatment, " ", ss$Temperature, "C (", ss$Year, "yr) ", sep=""), stringsAsFactors=F);
+		
+		# Combined places
+		pdf(paste(output_prefix, ".", alpha_diversity,".boxplot.pdf", sep=""), width=4, height=6);
+		print(qplot(factor(id), value, data = ss, geom = "boxplot") + geom_jitter(position=position_jitter(w=0.1, h=0.1)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
+						labs(x="", y=paste("", alpha_diversity)) + 
+						ggtitle(paste(output_prefix, "\nAlpha Diversity=", alpha_diversity,sep="")));
 		dev.off();
-		
-		# Combined place
-		pdf(paste("combined_place.", alpha_diversity, ".", kingdom,".boxplot.pdf", sep=""), width=4, height=6);
-		print(qplot(factor(sample), value, data = ss, geom = "boxplot") + geom_jitter(position=position_jitter(w=0.1, h=0.1)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)))
-		dev.off();
-		
-		# Combined experiment
-		pdf(paste("combined_experiment.", alpha_diversity, ".", kingdom,".boxplot.pdf", sep=""), width=4, height=6);
-		print(qplot(factor(experiment), value, data = ss, geom = "boxplot") + geom_jitter(position=position_jitter(w=0.1, h=0.1)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)))
-		dev.off();
-		
-		
-		# Combined temperature
-		pdf(paste("combined_temperature.", alpha_diversity, ".", kingdom,".boxplot.pdf", sep=""), width=4, height=6);
-		print(qplot(factor(temperature), value, data = ss, geom = "boxplot") + geom_jitter(position=position_jitter(w=0.1, h=0.1)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)))
-		dev.off();
-		rm(ss);			
 	}
 }
 
 
 
-# Usage: 
-# 
-main <- function(path, mean_replicate=False)
-{
-	# Prepare for importing data
-	kingdoms <- c("bacteria", "archaea");
-	alpha_diversities <- c("PD_whole_tree", "chao1", "fisher_alpha", "shannon", "observed_species");
-	
-	# Discard some columns from the datasets
-	col_to_be_removed <- c("X", "sequences.per.sample", "iteration", "H.GZ", "H.SHX")
-	sample_to_be_ignored <- "gDNA"
-	
-	# Characters used to delimit id
-	ids_delim_chars <- "[.|_]";
-
-	# flag: take a mean value from replicates
-	flag_mean = T
-	
-	# Data split into two kingdoms, import them separately
-	dfs<- list()
-	for(kingdom in kingdoms)
-	{
-		# import each alpha diversity
-		for(alpha_diversity in alpha_diversities)
-		{
-			print(paste("Reading from ", kingdom, "/", alpha_diversity, ".txt", sep=""))
-			
-			# Id to the current datasets
-			id <- paste(kingdom, ".", alpha_diversity, sep="");
-			
-			# Import dataset
-			data <- read.table(paste(kingdom, "/", alpha_diversity, ".txt", sep=""), header=T, sep="\t", stringsAsFactors=F);
-			
-			# select the rows with a highest sequences per sample value
-			data <- subset(data, data$sequences.per.sample == max(data$sequences.per.sample));
-			
-			# Discard columns
-			data <- data[,-which(colnames(data) %in% col_to_be_removed)]
-			
-			
-			# Insert data into list
-			dfs[[id]] <- data;
-		}
-	}
-	
-	# Reshape dataframe
-	melt_dfs <- melt_df(dfs);
-	
-	# Discard rows with n/a values
-	melt_dfs <- melt_dfs[-which(melt_dfs$value == "n/a"), ]
-	
-	# Discard rows containing patterns enlisted in sample_to_be_ignored
-	melt_dfs <- melt_dfs[-which(grepl("gDNA", melt_dfs$variable) == TRUE), ]
-	
-	
-	# split variable column into something meaningful
-	# As strsplit looking for regexp pattern, we have to put the period into a bracket or to include a fixed flag
-	library(stringr)
-	meta_info <- str_split_fixed(paste(melt_dfs$class, melt_dfs$variable, sep="."), "[.]", 6);
-	
-	# Rename the colnames into something meaningful
-	colnames(meta_info) <- c("kingdom", "alpha_diversity", "sample", "experiment", "temperature", "replicate_id");
-	
-	# Merge the new columns into data.frame
-	melt_dfs <- cbind(melt_dfs, meta_info);
-	
-	# Tidy up ids by removing redundant columns
-	melt_dfs <- melt_dfs[, -which(colnames(melt_dfs) %in% c("iteration", "class"))]
-	
-	# Cast values into correct datatype 
-	melt_dfs$value <- as.double(melt_dfs$value);
-	
-	# Factorize values 
-	melt_dfs$variable <- factor(melt_dfs$variable);
-	melt_dfs$alpha_diversity <- factor(melt_dfs$alpha_diversity);
-	
-	
-	# Plot a histogram of observed values
-	pdf("all_values.histogram.pdf")
-	hist(as.double(melt_dfs$value), breaks=100, col="steelblue", xlab="Alpha Diversity (Combined all)", main="Spectrum of Alpha Diversity")
-	dev.off();
-	
-	
-	# Prepare important variables
-	kingdoms <- as.character(unique(melt_dfs$kingdom))
-	alpha_diversities <- as.character(unique(melt_dfs$alpha_diversity));
-	experiments <- as.character(unique(melt_dfs$experiment));
-	samples <- as.character(unique(melt_dfs$sample));
-	temperatures <- as.character(unique(melt_dfs$temperature))
-	
-	
-	# Combined all and fixed in default variables
-	for(kingdom in kingdoms)
-	{
-		# Filter data from selected kingdom
-		ss <- melt_dfs[melt_dfs$kingdom == kingdom, ];
-		
-		pdf(paste("all_values.", kingdom,".boxplot.pdf", sep=""), width=20, height=5);
-		print(qplot(factor(variable), value, data = ss, geom = "boxplot", fill=factor(alpha_diversity)))
-		# qplot(factor(variable), value, data = ss, geom = "boxplot", fill=factor(alpha_diversity)) + geom_jitter(position=position_jitter(w=0.1, h=0.1)) 
-		#p <- ggplot(ss, aes(factor(variable), value)) + geom_jitter();
-		#p + geom_boxplot(aes(fill=factor(alpha_diversity))) + theme(axis.text.x = element_text(angle = 90, hjust = 1));
-		dev.off();
-		rm(ss);
-	}
-	
-	
-	# Combined replicates 
-	for(kingdom in kingdoms)
-	{
-		# Go through all alpha diversities
-		for(alpha_diversity in alpha_diversities)
-		{
-			# Select data on basis of selected kingdom and alpha diversity
-			ss <- melt_dfs[melt_dfs$kingdom == kingdom & melt_dfs$alpha_diversity == alpha_diversity, ];
-			
-			#melt_ss <- melt(ss, id=c("sample", "experiment", "temperature"))
-			ss <- cbind(ss, id=paste(ss$sample, ss$experiment, ss$temperature, sep=":"))
-			
-			# Take a mean value from replicates
-			if(flag_mean)
-			{
-				require(plyr)
-				ss <- ddply(ss, colnames(ss)[which(colnames(ss) != "value")], summarise, value=mean(value))
-			}
-			
-			pdf(paste("combined_replicates.", alpha_diversity, ".", kingdom,".boxplot.pdf", sep=""), width=4, height=6);
-			print(qplot(factor(id), value, data = ss, geom = "boxplot") + geom_jitter(position=position_jitter(w=0.1, h=0.1)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)))
-			# qplot(factor(variable), value, data = ss, geom = "boxplot", fill=factor(alpha_diversity)) + geom_jitter(position=position_jitter(w=0.1, h=0.1)) 
-			#p <- ggplot(ss, aes(factor(variable), value)) + geom_jitter();
-			#p + geom_boxplot(aes(fill=factor(alpha_diversity))) + theme(axis.text.x = element_text(angle = 90, hjust = 1));
-			dev.off();
-			
-			# Combined place
-			pdf(paste("combined_place.", alpha_diversity, ".", kingdom,".boxplot.pdf", sep=""), width=4, height=6);
-			print(qplot(factor(sample), value, data = ss, geom = "boxplot") + geom_jitter(position=position_jitter(w=0.1, h=0.1)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)))
-			dev.off();
-			
-			# Combined experiment
-			pdf(paste("combined_experiment.", alpha_diversity, ".", kingdom,".boxplot.pdf", sep=""), width=4, height=6);
-			print(qplot(factor(experiment), value, data = ss, geom = "boxplot") + geom_jitter(position=position_jitter(w=0.1, h=0.1)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)))
-			dev.off();
-			
-			
-			# Combined temperature
-			pdf(paste("combined_temperature.", alpha_diversity, ".", kingdom,".boxplot.pdf", sep=""), width=4, height=6);
-			print(qplot(factor(temperature), value, data = ss, geom = "boxplot") + geom_jitter(position=position_jitter(w=0.1, h=0.1)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)))
-			dev.off();
-			rm(ss);			
-		}
-	}
-}
-
-
+#
+#
+## Usage: 
+## 
+#main <- function(path, mean_replicate=False)
+#{
+#	# Prepare for importing data
+#	kingdoms <- c("bacteria", "archaea");
+#	alpha_diversities <- c("PD_whole_tree", "chao1", "fisher_alpha", "shannon", "observed_species");
+#	
+#	# Discard some columns from the datasets
+#	col_to_be_removed <- c("X", "sequences.per.sample", "iteration", "H.GZ", "H.SHX")
+#	sample_to_be_ignored <- "gDNA"
+#	
+#	# Characters used to delimit id
+#	ids_delim_chars <- "[.|_]";
+#
+#	# flag: take a mean value from replicates
+#	flag_mean = T
+#	
+#	# Data split into two kingdoms, import them separately
+#	dfs<- list()
+#	for(kingdom in kingdoms)
+#	{
+#		# import each alpha diversity
+#		for(alpha_diversity in alpha_diversities)
+#		{
+#			print(paste("Reading from ", kingdom, "/", alpha_diversity, ".txt", sep=""))
+#			
+#			# Id to the current datasets
+#			id <- paste(kingdom, ".", alpha_diversity, sep="");
+#			
+#			# Import dataset
+#			data <- read.table(paste(kingdom, "/", alpha_diversity, ".txt", sep=""), header=T, sep="\t", stringsAsFactors=F);
+#			
+#			# select the rows with a highest sequences per sample value
+#			data <- subset(data, data$sequences.per.sample == max(data$sequences.per.sample));
+#			
+#			# Discard columns
+#			data <- data[,-which(colnames(data) %in% col_to_be_removed)];
+#			
+#			
+#			# Insert data into list
+#			dfs[[id]] <- data;
+#		}
+#	}
+#	
+#	# Reshape dataframe
+#	melt_dfs <- melt_df(dfs);
+#	
+#	# Discard rows with n/a values
+#	melt_dfs <- melt_dfs[-which(melt_dfs$value == "n/a"), ]
+#	
+#	# Discard rows containing patterns enlisted in sample_to_be_ignored
+#	melt_dfs <- melt_dfs[-which(grepl("gDNA", melt_dfs$variable) == TRUE), ]
+#	
+#	
+#	# split variable column into something meaningful
+#	# As strsplit looking for regexp pattern, we have to put the period into a bracket or to include a fixed flag
+#	library(stringr)
+#	meta_info <- str_split_fixed(paste(melt_dfs$class, melt_dfs$variable, sep="."), "[.]", 6);
+#	
+#	# Rename the colnames into something meaningful
+#	colnames(meta_info) <- c("kingdom", "alpha_diversity", "sample", "experiment", "temperature", "replicate_id");
+#	
+#	# Merge the new columns into data.frame
+#	melt_dfs <- cbind(melt_dfs, meta_info);
+#	
+#	# Tidy up ids by removing redundant columns
+#	melt_dfs <- melt_dfs[, -which(colnames(melt_dfs) %in% c("iteration", "class"))]
+#	
+#	# Cast values into correct datatype 
+#	melt_dfs$value <- as.double(melt_dfs$value);
+#	
+#	# Factorize values 
+#	melt_dfs$variable <- factor(melt_dfs$variable);
+#	melt_dfs$alpha_diversity <- factor(melt_dfs$alpha_diversity);
+#	
+#	
+#	# Plot a histogram of observed values
+#	pdf("all_values.histogram.pdf")
+#	hist(as.double(melt_dfs$value), breaks=100, col="steelblue", xlab="Alpha Diversity (Combined all)", main="Spectrum of Alpha Diversity")
+#	dev.off();
+#	
+#	
+#	# Prepare important variables
+#	kingdoms <- as.character(unique(melt_dfs$kingdom))
+#	alpha_diversities <- as.character(unique(melt_dfs$alpha_diversity));
+#	experiments <- as.character(unique(melt_dfs$experiment));
+#	samples <- as.character(unique(melt_dfs$sample));
+#	temperatures <- as.character(unique(melt_dfs$temperature))
+#	
+#	
+#	# Combined all and fixed in default variables
+#	for(kingdom in kingdoms)
+#	{
+#		# Filter data from selected kingdom
+#		ss <- melt_dfs[melt_dfs$kingdom == kingdom, ];
+#		
+#		pdf(paste("all_values.", kingdom,".boxplot.pdf", sep=""), width=20, height=5);
+#		print(qplot(factor(variable), value, data = ss, geom = "boxplot", fill=factor(alpha_diversity)))
+#		# qplot(factor(variable), value, data = ss, geom = "boxplot", fill=factor(alpha_diversity)) + geom_jitter(position=position_jitter(w=0.1, h=0.1)) 
+#		#p <- ggplot(ss, aes(factor(variable), value)) + geom_jitter();
+#		#p + geom_boxplot(aes(fill=factor(alpha_diversity))) + theme(axis.text.x = element_text(angle = 90, hjust = 1));
+#		dev.off();
+#		rm(ss);
+#	}
+#	
+#	
+#	# Combined replicates 
+#	for(kingdom in kingdoms)
+#	{
+#		# Go through all alpha diversities
+#		for(alpha_diversity in alpha_diversities)
+#		{
+#			# Select data on basis of selected kingdom and alpha diversity
+#			ss <- melt_dfs[melt_dfs$kingdom == kingdom & melt_dfs$alpha_diversity == alpha_diversity, ];
+#			
+#			#melt_ss <- melt(ss, id=c("sample", "experiment", "temperature"))
+#			ss <- cbind(ss, id=paste(ss$sample, ss$experiment, ss$temperature, sep=":"))
+#			
+#			# Take a mean value from replicates
+#			if(flag_mean)
+#			{
+#				require(plyr)
+#				ss <- ddply(ss, colnames(ss)[which(colnames(ss) != "value")], summarise, value=mean(value))
+#			}
+#			
+#			pdf(paste("combined_replicates.", alpha_diversity, ".", kingdom,".boxplot.pdf", sep=""), width=4, height=6);
+#			print(qplot(factor(id), value, data = ss, geom = "boxplot") + geom_jitter(position=position_jitter(w=0.1, h=0.1)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)))
+#			# qplot(factor(variable), value, data = ss, geom = "boxplot", fill=factor(alpha_diversity)) + geom_jitter(position=position_jitter(w=0.1, h=0.1)) 
+#			#p <- ggplot(ss, aes(factor(variable), value)) + geom_jitter();
+#			#p + geom_boxplot(aes(fill=factor(alpha_diversity))) + theme(axis.text.x = element_text(angle = 90, hjust = 1));
+#			dev.off();
+#			
+#			# Combined place
+#			pdf(paste("combined_place.", alpha_diversity, ".", kingdom,".boxplot.pdf", sep=""), width=4, height=6);
+#			print(qplot(factor(sample), value, data = ss, geom = "boxplot") + geom_jitter(position=position_jitter(w=0.1, h=0.1)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)))
+#			dev.off();
+#			
+#			# Combined experiment
+#			pdf(paste("combined_experiment.", alpha_diversity, ".", kingdom,".boxplot.pdf", sep=""), width=4, height=6);
+#			print(qplot(factor(experiment), value, data = ss, geom = "boxplot") + geom_jitter(position=position_jitter(w=0.1, h=0.1)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)))
+#			dev.off();
+#			
+#			
+#			# Combined temperature
+#			pdf(paste("combined_temperature.", alpha_diversity, ".", kingdom,".boxplot.pdf", sep=""), width=4, height=6);
+#			print(qplot(factor(temperature), value, data = ss, geom = "boxplot") + geom_jitter(position=position_jitter(w=0.1, h=0.1)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)))
+#			dev.off();
+#			rm(ss);			
+#		}
+#	}
+#}
+#
+#
